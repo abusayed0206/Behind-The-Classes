8c473b0ef79770cb27d66966058e6b2cef3fda42
MD. ABU SAYED <abusayed0206@outlook.com>
2021-10-16 12:32:14 +0600

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,16 +1,17 @@
 @extends('_layouts.master')
 
-@section('title', 'About')
+@section('title', 'About Dev')
 
 @section('content')
-    <h1>About</h1>
+    <h1>About Dev</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
-
+    <p>My name is {{ $page->owner->name }}. I built this site with gatsby template by Ranie Santos. I am not affiliated with "Behind The Classes" team! If You're from Behind the classes team please directly contact me at contact@abusayed.dev or contact@behindtheclasses.com </p>
+    <p> If you wanna claim this domain copyright you have to be 'legal copyright owner' of 'Behind the Classes' Or, you can negotiate with me directly @lrsayed_bot (Telegram) </p>
     <h2>Links:</h2>
 
     <ul>
         <li><a href="/twitter" target="_blank">Twitter</a></li>
         <li><a href="/github" target="_blank">GitHub</a></li>
+        <li><a href="https://abusayed.dev">Website</a></li> 
     </ul>
 @endsection

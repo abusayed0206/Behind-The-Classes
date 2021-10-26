dc8f0c14332092ea7966a5ee064574cd561ead35
MD. ABU SAYED <abusayed0206@outlook.com>
2021-10-27 00:10:08 +0600

    Test

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome Behind The Classes🌼</h1>
+    <h1>Welcome to Behind The Classes🌼</h1>
 
     <p>Behind The Classes is a reliable source/platform for online education for SSC,HSC & Admission students of Bangladesh.</p>
     <p> This domain is not owned by "Behind The Classes" Team </p>
